00d902b38ec285a68b847aaa30d3310595a3fca6
Ernesto Medina <ernestomedina17@gmail.com>
2023-05-25 21:54:14 +0200

    saving money, EOD

--- IaC/resources.tf
+++ IaC/resources.tf
@@ -1,18 +1,19 @@
-resource "aws_eks_cluster" "unicron" {
-  name     = "unicron"
-  role_arn = aws_iam_role.unicron.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.main_a.id, aws_subnet.main_b.id, aws_subnet.main_c.id]
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.unicron-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.unicron-AmazonEKSVPCResourceController,
-  ]
-}
+# To save some money
+#resource "aws_eks_cluster" "unicron" {
+#  name     = "unicron"
+#  role_arn = aws_iam_role.unicron.arn
+#
+#  vpc_config {
+#    subnet_ids = [aws_subnet.main_a.id, aws_subnet.main_b.id, aws_subnet.main_c.id]
+#  }
+#
+#  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+#  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+#  depends_on = [
+#    aws_iam_role_policy_attachment.unicron-AmazonEKSClusterPolicy,
+#    aws_iam_role_policy_attachment.unicron-AmazonEKSVPCResourceController,
+#  ]
+#}
 
 data "aws_iam_policy_document" "eks_assume_role" {
   statement {
