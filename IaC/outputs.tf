1578c77052dbd66d7d875fcb25d6e0809a7667ab
Ernesto Medina <ernestomedina17@gmail.com>
2023-05-25 21:46:44 +0200

    removing ipv6 output

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -15,49 +15,45 @@ output "igw_id" {
 }
 
 output "unicron_eks_arn" {
-  value = aws_eks_cluster.arn
+  value = aws_eks_cluster.unicron.arn
 }
 
 output "unicron_eks_ca" {
-  value = aws_eks_cluster.certificate_authority
+  value = aws_eks_cluster.unicron.certificate_authority
 }
 
 output "unicron_eks_id" {
-  value = aws_eks_cluster.cluster_id
+  value = aws_eks_cluster.unicron.cluster_id
 }
 
 output "unicron_eks_created_at" {
-  value = aws_eks_cluster.created_at
+  value = aws_eks_cluster.unicron.created_at
 }
 
 output "unicron_eks_endpoint" {
-  value = aws_eks_cluster.endpoint
+  value = aws_eks_cluster.unicron.endpoint
 }
 
 output "unicron_eks_name" {
-  value = aws_eks_cluster.id
+  value = aws_eks_cluster.unicron.id
 }
 
 output "unicron_eks_identity" {
-  value = aws_eks_cluster.identity
-}
-
-output "unicron_eks_ipv6" {
-  value = aws_eks_cluster.kubernetes_network_config.service_ipv6_cidr
+  value = aws_eks_cluster.unicron.identity
 }
 
 output "unicron_eks_version" {
-  value = aws_eks_cluster.platform_version
+  value = aws_eks_cluster.unicron.platform_version
 }
 
 output "unicron_eks_status" {
-  value = aws_eks_cluster.status
+  value = aws_eks_cluster.unicron.status
 }
 
 output "unicron_eks_tags_all" {
-  value = aws_eks_cluster.tags_all
+  value = aws_eks_cluster.unicron.tags_all
 }
 
 output "unicron_eks_vpc_config" {
-  value = aws_eks_cluster.vpc_config
+  value = aws_eks_cluster.unicron.vpc_config
 }
