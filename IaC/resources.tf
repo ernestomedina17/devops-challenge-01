f50528bec3369545b41b5ea06574d1ee5ced5524
Ernesto Medina <ernestomedina17@gmail.com>
2023-05-25 20:39:46 +0200

    bugfix

---
+++ IaC/resources.tf
@@ -0,0 +1,45 @@
+resource "aws_eks_cluster" "unicron" {
+  name     = "unicron"
+  role_arn = aws_iam_role.unicron.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.main_a.id, aws_subnet.main_b.id, aws_subnet.main_c.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.unicron-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.unicron-AmazonEKSVPCResourceController,
+  ]
+}
+
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "unicron" {
+  name               = "eks-cluster-unicron"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "unicron-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam:${var.aws_account_id}:aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.unicron.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "unicron-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam:${var.aws_account_id}:aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.unicron.name
+}
--- IaC/variables.tf
+++ IaC/variables.tf
@@ -1,3 +1,6 @@
 variable "environment" {}
 variable "aws_region" {}
 variable "availability_zone_names" {}
+variable "aws_account_id" {
+  description = "TF_VAR_aws_account_id is a zsh env variable"
+}
