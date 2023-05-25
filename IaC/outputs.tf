412514310d58d655f29643359f301daa8d92850e
Ernesto Medina <ernestomedina17@gmail.com>
2023-05-25 19:39:43 +0200

    bugfix

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -1,11 +1,11 @@
-output "subnet_a_availability_zone_id {
-  aws_subnet.main_a.availability_zone_id
+output "subnet_a_availability_zone_id" {
+  value = aws_subnet.main_a.availability_zone_id
 }
 
-output "subnet_b_availability_zone_id {
-  aws_subnet.main_b.availability_zone_id
+output "subnet_b_availability_zone_id" {
+  value = aws_subnet.main_b.availability_zone_id
 }
 
-output "subnet_c_availability_zone_id {
-  aws_subnet.main_c.availability_zone_id
+output "subnet_c_availability_zone_id" {
+  value = aws_subnet.main_c.availability_zone_id
 }
