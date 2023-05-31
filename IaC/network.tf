d5a32d1ec717a273f6dd851dacae860874da664e
Ernesto Medina <ernestomedina17@gmail.com>
2023-05-31 12:37:33 +0200

    splitting project's network resources from default ones created by AWS

--- IaC/network.tf
+++ IaC/network.tf
@@ -7,57 +7,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-# This resource cannot be destroyed, only managed.
-resource "aws_default_network_acl" "default" {
-  default_network_acl_id = aws_vpc.main.default_network_acl_id
-  subnet_ids             = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.private_b.id, aws_subnet.private_c.id]
-
-  tags = {
-    Name = "Unicron Default"
-  }
-
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-}
-
-# This resource cannot be destroyed, only managed.
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Unicron Default"
-  }
-
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "192.168.0.0/18"
@@ -123,15 +72,6 @@ resource "aws_nat_gateway" "private_b" {
   subnet_id         = aws_subnet.private_b.id
   tags = {
     Scope = "Private"
-  }
-}
-
-# This resource cannot be destroyed, only managed.
-resource "aws_default_route_table" "default" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
-
-  tags = {
-    Name = "Unicron Default"
   }
 }
 
