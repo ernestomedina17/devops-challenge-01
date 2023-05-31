fc3eca80e9f224dd8dc4647a9c7f14ea61375817
Ernesto Medina <ernestomedina17@gmail.com>
2023-05-31 12:42:47 +0200

    splitting project's network resources from default ones created by AWS

--- IaC/default.tf
+++ IaC/default.tf
@@ -8,27 +8,27 @@ resource "aws_default_vpc" "default" {
   }
 }
 
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "euc1-az1"
-
-  tags = {
-    Name = "Default AZ1"
-  }
-}
-
 resource "aws_default_subnet" "default_az2" {
-  availability_zone = "euc1-az2"
+  availability_zone = "eu-central-1a"
 
   tags = {
-    Name = "Default AZ2"
+    Name = "Default 1A"
   }
 }
 
 resource "aws_default_subnet" "default_az3" {
-  availability_zone = "euc1-az3"
+  availability_zone = "eu-central-1b"
 
   tags = {
-    Name = "Default AZ3"
+    Name = "Default 1B"
+  }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "eu-central-1c"
+
+  tags = {
+    Name = "Default 1C"
   }
 }
 
