991793c5df5686c274771c2649e84375413f330e
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-05 23:27:54 +0200

    az_counts bugfix

--- IaC/resources.tf
+++ IaC/resources.tf
@@ -1,12 +1,11 @@
 module "network" {
-  #source = "git@github.com:ernestomedina17/tf-modules.git//aws/4/eks-vpc"
-  source = "../../tf-modules/aws/4/eks-vpc"
+  source = "git@github.com:ernestomedina17/tf-modules.git//aws/4/eks-vpc"
 
   name               = "unicron"
   cidr_block         = "10.0.0.0/16"
   region             = var.aws_region
   availability_zones = data.aws_availability_zones.available.names
-  az_counts          = length([data.aws_availability_zones.available.names])
+  az_counts          = length(data.aws_availability_zones.available.names)
 }
 
 resource "aws_kms_key" "unicron" {
