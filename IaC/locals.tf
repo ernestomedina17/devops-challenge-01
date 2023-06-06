d9ef728b42ce19fec48929c8df7f48853e5dc785
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-06 12:56:08 +0200

    destroying k8s

--- IaC/locals.tf
+++ IaC/locals.tf
@@ -6,18 +6,18 @@ locals {
   eks_al2_ami_id                       = data.aws_ssm_parameter.eks_al2_ami_id.value
   eks_al2_ami_release_version          = nonsensitive(data.aws_ssm_parameter.eks_al2_ami_release_version.value)
 
-  config_map_aws_auth = <<CONFIGMAPAWSAUTH
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: ${aws_iam_role.decepticons.arn}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-CONFIGMAPAWSAUTH
+  #  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+  #apiVersion: v1
+  #kind: ConfigMap
+  #metadata:
+  #  name: aws-auth
+  #  namespace: kube-system
+  #data:
+  #  mapRoles: |
+  #    - rolearn: ${aws_iam_role.decepticons.arn}
+  #      username: system:node:{{EC2PrivateDNSName}}
+  #      groups:
+  #        - system:bootstrappers
+  #        - system:nodes
+  #CONFIGMAPAWSAUTH
 }
