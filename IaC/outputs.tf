4d3529f5205b2cd237c88e49c685af7030ff765f
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-06 10:17:42 +0200

    destroying kms resource

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -1,19 +1,19 @@
-output "vpc_network_id" {
-  value = module.network.vpc_network_id
-}
-
-output "private_subnet_id" {
-  value = module.network.private_subnet_id
-}
-
-output "public_subnet_id" {
-  value = module.network.public_subnet_id
-}
-
-output "az_names" {
-  value = data.aws_availability_zones.available.names
-}
-
-output "az_count" {
-  value = length(data.aws_availability_zones.available.names)
-}
+#output "vpc_network_id" {
+#  value = module.network.vpc_network_id
+#}
+#
+#output "private_subnet_id" {
+#  value = module.network.private_subnet_id
+#}
+#
+#output "public_subnet_id" {
+#  value = module.network.public_subnet_id
+#}
+#
+#output "az_names" {
+#  value = data.aws_availability_zones.available.names
+#}
+#
+#output "az_count" {
+#  value = length(data.aws_availability_zones.available.names)
+#}
--- IaC/resources.tf
+++ IaC/resources.tf
@@ -1,34 +1,10 @@
-module "network" {
-  source = "git@github.com:ernestomedina17/tf-modules.git//aws/4/eks-vpc"
+#module "network" {
+#  source = "git@github.com:ernestomedina17/tf-modules.git//aws/4/eks-vpc"
+#
+#  name               = "unicron"
+#  cidr_block         = "10.0.0.0/16"
+#  region             = var.aws_region
+#  availability_zones = data.aws_availability_zones.available.names
+#  az_counts          = length(data.aws_availability_zones.available.names)
+#}
 
-  name               = "unicron"
-  cidr_block         = "10.0.0.0/16"
-  region             = var.aws_region
-  availability_zones = data.aws_availability_zones.available.names
-  az_counts          = length(data.aws_availability_zones.available.names)
-}
-
-resource "aws_kms_key" "unicron" {
-  description = "unicron's kms key"
-}
-
-resource "aws_kms_key_policy" "unicron" {
-  key_id = aws_kms_key.unicron.id
-  policy = jsonencode({
-    Id = "unicron"
-    Statement = [
-      {
-        # Resource policy 
-        Action = "kms:*"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${var.aws_account_id}:root"
-        }
-
-        Resource = "*"
-        Sid      = "Allows IAM policies to allow access to the KMS key."
-      },
-    ]
-    Version = "2012-10-17"
-  })
-}
