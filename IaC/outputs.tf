558455dfbd96e5dc315768b454c9ccb05992fe2e
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-06 10:53:00 +0200

    merging files and cleaning up

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -21,3 +21,23 @@ output "az_count" {
 output "kms_arn" {
   value = module.kms.kms_key_arn
 }
+
+output "unicron_eks_endpoint" {
+  value = aws_eks_cluster.unicron.endpoint
+}
+
+output "unicron_eks_identity" {
+  value = aws_eks_cluster.unicron.identity
+}
+
+output "unicron_eks_version" {
+  value = aws_eks_cluster.unicron.platform_version
+}
+
+output "unicron_eks_status" {
+  value = aws_eks_cluster.unicron.status
+}
+
+output "unicron_eks_vpc_config" {
+  value = aws_eks_cluster.unicron.vpc_config
+}
