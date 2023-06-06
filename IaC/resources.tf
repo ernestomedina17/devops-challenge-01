4d17091fe7929540e6298f8d470ce70ddf11035d
Ernesto Medina <ernestomedina17@gmail.com>
2023-06-06 10:30:14 +0200

    replacing kms resource with module

--- IaC/resources.tf
+++ IaC/resources.tf
@@ -1,10 +1,15 @@
 #module "network" {
 #  source = "git@github.com:ernestomedina17/tf-modules.git//aws/4/eks-vpc"
-#
-#  name               = "unicron"
+#  name               = local.name
 #  cidr_block         = "10.0.0.0/16"
 #  region             = var.aws_region
 #  availability_zones = data.aws_availability_zones.available.names
 #  az_counts          = length(data.aws_availability_zones.available.names)
 #}
+
+module "kms" {
+  source         = "git@github.com:ernestomedina17/tf-modules.git//aws/4/kms"
+  name           = local.name
+  aws_account_id = var.aws_account_id
+}
 
